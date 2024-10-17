19ccc39303e45ac7c9744d25af381cdfecd28b6a
stringcode <michael@stringcode.co.uk>
2024-10-17 19:20:03 +0300

    button fixes

--- src/components/showcase/ReviewCode.jsx
+++ src/components/showcase/ReviewCode.jsx
@@ -1,8 +1,10 @@
-import styled, { css } from 'styled-components'
+import styled, {css} from 'styled-components'
 import Theme from "../../theme/Theme.js";
 import OneAppSixWays from "./OneAppSixWays.jsx";
 import Section from "./Section.jsx";
 
+
+const GITHUB_URL = "https://github.com/stringcode86/coinwatch"
 
 const reviewCodeStyle = {
     display: "flex",
@@ -10,8 +12,8 @@ const reviewCodeStyle = {
 }
 
 const OneAppSixWaysWrapper = styled.div`
-    @media only screen and (max-width : 767px) {
-        display: none;
+    @media only screen and (max-width: 767px) {
+      display: none;
     }
 `
 
@@ -21,12 +23,14 @@ const ReviewItemsWrapper = styled.div`
     flex-direction: column;
 `
 
-const ReviewCodeItem = styled.div`
+const ReviewCodeItem = styled.button`
+    text-align: start;
+    
     > h2 {
         color: ${Theme.colors.primary};
         margin-bottom: 0.25em;
     }
-
+    
     &:hover > h2:after {
         content: ' >';
     }
@@ -36,31 +40,31 @@ const ReviewCode = ({id}) => {
     return <Section id={id} style={reviewCodeStyle}>
         <OneAppSixWaysWrapper><OneAppSixWays/></OneAppSixWaysWrapper>
         <ReviewItemsWrapper>
-            <ReviewCodeItem>
+            <ReviewCodeItem onClick={() => window.open(GITHUB_URL, '_blank')}>
                 <h2>Review Code</h2>
                 <div className="ReviewCode">
                     During the interview process there is often a task. Coin Watch is simple app that
                     is a good example of such task. Written in six different ways. App allows user to
                     view coins and various details about them. It allows user to bookmark coins. Uses
                     coin gecko API. Review the code
-                    on <a href="https://github.com/stringcode86/coinwatch" target="_blank">GitHub</a>.
+                    on <a href={GITHUB_URL} target="_blank">GitHub</a>.
                 </div>
             </ReviewCodeItem>
-            <ReviewCodeItem>
+            <ReviewCodeItem onClick={() => window.open(GITHUB_URL, '_blank')}>
                 <h2>Web front end</h2>
                 <div className="ReviewCode">
                     React.js, Redux front end app version of Coin Watch,
                     review <a href="https://github.com/stringcode86/coinwatch" target="_blank">here</a>.
                 </div>
             </ReviewCodeItem>
-            <ReviewCodeItem>
+            <ReviewCodeItem onClick={() => window.open(GITHUB_URL, '_blank')}>
                 <h2>Mobile React Native (iOS & Android)</h2>
                 <div className="ReviewCode">
                     React Native app version of Coin Watch for iOS and Android.
                     review <a href="https://github.com/stringcode86/coinwatch" target="_blank">here</a>.
                 </div>
             </ReviewCodeItem>
-            <ReviewCodeItem>
+            <ReviewCodeItem onClick={() => window.open(GITHUB_URL, '_blank')}>
                 <h2>iOS UIKit (with KMM)</h2>
                 <div className="ReviewCode">
                     Kotlin multi-platform version of the app. Share core library that contains all the
@@ -70,14 +74,14 @@ const ReviewCode = ({id}) => {
                     code <a href="https://github.com/stringcode86/coinwatch" target="_blank">here</a>.
                 </div>
             </ReviewCodeItem>
-            <ReviewCodeItem>
+            <ReviewCodeItem onClick={() => window.open(GITHUB_URL, '_blank')}>
                 <h2>iOS Swift UI</h2>
                 <div className="ReviewCode">
                     Pure SwiftUI version of the app. Uses all the SwiftUI latest best practices. Review
                     code <a href="https://github.com/stringcode86/coinwatch" target="_blank">here</a>.
                 </div>
             </ReviewCodeItem>
-            <ReviewCodeItem>
+            <ReviewCodeItem onClick={() => window.open(GITHUB_URL, '_blank')}>
                 <h2>Android View based (with KMM)</h2>
                 <div className="ReviewCode">
                     Kotlin multi-platform version of the app. Share core library that contains all the
@@ -87,7 +91,7 @@ const ReviewCode = ({id}) => {
                     code <a href="https://github.com/stringcode86/coinwatch" target="_blank">here</a>.
                 </div>
             </ReviewCodeItem>
-            <ReviewCodeItem>
+            <ReviewCodeItem onClick={() => window.open(GITHUB_URL, '_blank')}>
                 <h2>Android Jetpack Compose</h2>
                 <div className="ReviewCode">
                     Pure Jetpack Compose version of the app. Uses all the Jetpack Compose latest best practices.
