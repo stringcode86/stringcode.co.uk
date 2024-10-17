82a188590dd365df3052a1d1defbe403cb405df8
stringcode <michael@stringcode.co.uk>
2024-10-17 15:18:33 +0300

    Fixing Error Boundry

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -7,21 +7,19 @@ class ErrorBoundary extends React.Component {
     }
 
     static getDerivedStateFromError(error) {
-        // Update state so the next render will show the fallback UI.
         return { hasError: true }
     }
 
     componentDidCatch(error, errorInfo) {
-        // You can also log the error to an error reporting service
-        logErrorToMyService(error, errorInfo)
+        console.log('[ERROR]', error)
+        console.log('[ERROR]', errorInfo)
     }
 
     render() {
         if (this.state.hasError) {
-            // You can render any custom fallback UI
             return <h1>
                 Ngl it ain't looking good chief. Maybe try enabling webgl, or
-                disabling aggressive privacy protections. Anon cypher punk you.
+                disabling aggressive privacy protections. You anon cypher punk you.
             </h1>
         }
 
